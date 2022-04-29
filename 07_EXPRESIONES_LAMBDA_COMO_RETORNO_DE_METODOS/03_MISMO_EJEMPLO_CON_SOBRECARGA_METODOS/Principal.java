b1bc8c801f5779dda1d6da9e6766bd1234e7809e
SOLMAR505 <reyesmarisol018@gmail.com>
2022-04-29 10:27:29 -0500

    Cambios y correcciones

--- 07_EXPRESIONES_LAMBDA_COMO_RETORNO_DE_METODOS/03_MISMO_EJEMPLO_CON_SOBRECARGA_METODOS/Principal.java
+++ 07_EXPRESIONES_LAMBDA_COMO_RETORNO_DE_METODOS/03_MISMO_EJEMPLO_CON_SOBRECARGA_METODOS/Principal.java
@@ -5,15 +5,16 @@ public class Principal {
         System.out.println("==> CalculadoraInt --> resultado = " + Principal.engine_1().calculate(5,5));
         System.out.println("==> CalculadoraLong --> resultado = " + Principal.engine_2().calculate(6,2));
 
-        System.out.println("==> CalculadoraInt --> resultado = " + Principal.engine_0(x1, y1).calculate(5,4));
+        System.out.println("==> CalculadoraInt --> resultado = " + Principal.engine_0(5, 2).calculate(5,4));
     }
     //retorna un objeto de tipo "CalculadoraInt"
     public static CalculadoraInt engine_1(){
         return (x,y) -> x * y;
     }
     public static CalculadoraInt engine_0(int x1, int y1){
-        System.out.println("Parametros: " +x1+"," +y1);
-        return (x1,y1) -> x1 + y1;
+
+        System.out.println("Parametros de la suma: " + x1 +"," +y1);
+        return (x,y) -> x + y;
     }
      
      //Retorna un objetode tipo CalculadoraLong
